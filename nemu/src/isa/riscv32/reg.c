1b3318750607f8827beba7fe802870cce2e1c54d
tracer-ics2019 <tracer@njuics.org>
2021-01-05 06:49:45 -0800

    >  compile U201711615 apple Linux ubuntu 5.4.0-58-generic #64~18.04.1-Ubuntu SMP Wed Dec 9 17:11:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:49:45 up 2 days,  1:32,  1 user,  load average: 0.60, 0.36, 0.23 90dff04bdbbbc247c2eefaf47bea0d854b55c07c

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -1,5 +1,7 @@
 #include "nemu.h"
 
+#define REGISTER_NUMBER 32
+
 const char *regsl[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
@@ -13,10 +15,34 @@ void isa_reg_display() {
   	for(int i = 0; i < regNum; i++)
   	{
     	reg = reg_l(i);
-    	printf("%-6s 0x%8x %9d\n", reg_name(i, sizeof(reg)*8), reg, reg);
+    	printf("%-6s 0x%x %9d\n", reg_name(i, sizeof(reg)*8), reg, reg);
   	}
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
-  return 0;
+	*success = true;
+	if (strcmp(s,"pc") == 0)
+	{
+		return cpu.pc;
+	}
+	else if (s[0] == '0')
+	{
+		return reg_l(0);
+	}
+
+	for (int i = 0; i < REGISTER_NUMBER; ++i)
+	{
+		if (strcmp(regsl[i], s) == 0)
+		{
+			break;
+		}
+	}
+
+	if (i < 32)
+	{
+		return reg_l(i);
+	}
+
+	*success = false;
+  	return 0;
 }
--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,12 +5,14 @@
  */
 #include <sys/types.h>
 #include <regex.h>
+#include <stdlib.h>
+
+uint32_t isa_reg_str2val(const char *s, bool *success);
+uint32_t paddr_read(paddr_t addr, int len);
 
 enum {
-  TK_NOTYPE = 256, TK_EQ
-
-  /* TODO: Add more token types */
-
+  TK_NOTYPE = 256, TK_EQ, TK_PLUS, 
+  TK_REG, TK_DNUM, TK_HNUM, TK_NEQ, TK_AND, TK_DEREF, TK_NEG
 };
 
 static struct rule {
@@ -23,8 +25,21 @@ static struct rule {
    */
 
   {" +", TK_NOTYPE},    // spaces
+  {"==", TK_EQ},         // equal
   {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+
+
+  {"^\\$0|^\\$ra|^\\$[sgt]p|^\\$t[0-6]|^\\$s[0-9]|^\\$s1[01]|^\\$a[0-7]|^\\$pc", TK_REG}, // regex 
+  {"\\b[0-9]+\\b", TK_DNUM}, // decimal
+  {"0[xX][0-9a-fA-F]+", TK_HNUM}, // hex
+  {"-", '-'}, // minus
+  {"\\*", '*'}, // multiple
+  {"\\/", '/'}, // divide
+  {"\\(", '('}, // left brac
+  {"\\)", ')'}, // right brac
+  {"!=", TK_NEQ}, // not equal
+  {"&&", TK_AND} // and
+
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -56,12 +71,25 @@ typedef struct token {
 static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
+static bool check_parents(uint32_t p, uint32_t q);
+static uint32_t operation_position(uint32_t p, uint32_t q);
+static uint32_t eval(uint32_t p, uint32_t q);
+static bool judge_deref(int type);
+static bool judge_neg(int type);
+
+
 static bool make_token(char *e) {
   int position = 0;
   int i;
   regmatch_t pmatch;
 
   nr_token = 0;
+
+  for (int j = 0; j < NR_REGEX; ++j)
+  {
+    memset(tokens[j].str, '\0', 32);
+    tokens[j].type = 0;
+  }
 
   while (e[position] != '\0') {
     /* Try all rules one by one. */
@@ -79,7 +107,35 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
 
+        if (rules[i].token_type == TK_NOTYPE)
+        {
+          break;
+        }
+
         switch (rules[i].token_type) {
+          case TK_DNUM:
+          case TK_HNUM:
+            if (substr_len > 32)
+            {
+              printf("ERROR, overflowed bit number\n");
+              assert(0);
+            }
+
+          case '+':
+          case '-':
+          case '*':
+          case '/':
+          case '(':
+          case ')':
+          case TK_EQ:
+          case TK_REG:
+          case TK_NEQ:
+          case TK_AND:
+            strncpy(tokens[nr_token].str, substr_start, substr_len);
+            tokens[nr_token].type = rules[i].token_type;
+            nr_token++;
+            break;
+
           default: TODO();
         }
 
@@ -96,14 +152,179 @@ static bool make_token(char *e) {
   return true;
 }
 
+static bool check_parents(uint32_t p, uint32_t q){
+  int flag = 0;
+  if (strcmp(tokens[p].str, "(") || strcmp(tokens[q].str, ")"))
+  {
+    return false;
+  }
+
+  p++;
+  while(p<q){
+    if (strcmp(tokens[p].str, "(") == 0)
+    {
+      p++;
+      flag++;
+    }else if (strcmp(tokens[p].str, ")") == 0)
+    {
+      p++;
+      flag--;
+      if (flag < 0)
+      {
+        return false;
+      }
+    }else p++;
+  }
+  return flag == 0;
+}
+
+static uint32_t operation_position(uint32_t p, uint32_t q){
+  assert(p <= q);
+  uint32_t i = p;
+  uint32_t op = p;
+  int pnum = 0;
+
+  for (; i < q; ++i)
+  {
+    if (strcmp(tokens[i].str, "(") == 0)
+    {
+      pnum++;
+    }
+
+    if (strcmp(tokens[i].str, ")") == 0)
+    {
+      pnum--;
+    }
+
+    if (pnum == 0){
+      if (strcmp(tokens[i].str, "+") == 0 || strcmp(tokens[i].str, "-") == 0 ||
+        strcmp(tokens[i].str, "*") == 0 || strcmp(tokens[i].str, "/") == 0 ||
+            tokens[i].type == TK_DEREF|| tokens[i].type == TK_EQ || 
+            tokens[i].type == TK_NEQ ||  tokens[i].type == TK_AND) 
+      {
+        op = i; 
+        break;
+      }
+    }
+  }
+  return op;
+}
+
+static uint32_t eval(uint32_t p, uint32_t q){
+  if (p > q)
+  {
+    printf("ERROR\n");
+    assert(0);
+  }
+
+  if (p == q)
+  {
+    uint32_t value = 0;
+    if (tokens[p].type == TK_DNUM)
+    {
+      value = strtoul(tokens[p].str, NULL, 10);
+    }
+
+    if (tokens[p].type == TK_HNUM)
+    {
+      value = strtoul(tokens[p].str, NULL, 16);
+    }
+
+    if (tokens[p].type == TK_REG)
+    {
+      for (int i = 0; i < strlen(tokens[p].str); ++i)
+      {
+        tokens[p].str[i] = tokens[p].str[i+1];
+      }
+
+      bool success = true;
+      value = isa_reg_str2val(tokens[p].str, &success);
+    }
+    return value;
+  }else if (check_parents(p, q) == true)
+  {
+    printf("check parents correctly, p = %u, q = %u\n", p, q);
+    return eval(p+1, q-1);
+  }else{
+    int op = operation_position(p, q);
+    uint32_t temp = 0;
+    if (tokens[op].type != TK_DEREF && tokens[p].type != TK_NEG)
+    {
+      temp = eval(p, op-1);
+    }
+    uint32_t temp1 = eval(op+1, q);
+
+    switch(tokens[op].type){
+      case '+': 
+        return temp + temp1;
+      case '-':
+        return temp - temp1;
+      case '*': 
+        return temp * temp1;
+      case '/':
+        return temp / temp1;
+      case TK_DEREF:
+        return paddr_read(temp1, 4);
+      case TK_EQ:
+        return temp == temp1;
+      case TK_NEQ:
+        return temp != temp1;
+      case TK_AND:
+        return temp && temp1;
+      case TK_NEG:
+        return -temp1;
+      default:
+        assert(0);
+    }
+  }
+  return 0;
+}
+
+static bool judge_deref(int type){
+  switch(type) {
+    case '+':
+    case '-':
+    case '*':
+    case '/': 
+      return true; 
+    default : 
+      return false;
+  }
+}
+
+static bool judge_neg(int type){
+  switch(type) {
+    case '+':
+    case '-':
+    case '*':
+    case '/': 
+    case '(': 
+      return true; 
+    default : 
+      return false;
+  }
+}
+
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
 
-  /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  *success = true;
+  for (int i = 0; i < nr_token; ++i)
+  {
+    if (tokens[i].type == '*' && (i == 0 || judge_deref(tokens[i-1].type)))
+    {
+      tokens[i].type = TK_DEREF;
+    }
 
-  return 0;
+    if (tokens[i].type == '-' && (i == 0 || judge_neg(tokens[i-1].type)))
+    {
+      tokens[i].type = TK_NEG;
+    }
+  }
+
+  uint32_t result = eval(0, nr_token-1);
+  return result;
 }
