62e6898eb11fa422d0c550039d7d37ecd8588245
tracer-ics2019 <tracer@njuics.org>
2021-01-10 15:44:08 -0800

    >  compile U201711615 apple Linux ubuntu 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:44:08 up 1 day,  3:45,  1 user,  load average: 0.15, 0.23, 0.20 6fd5e3e08296a91a6e0731d51f6fa6aec6fb7fa

--- nemu/src/isa/riscv32/exec/ldst.c
+++ nemu/src/isa/riscv32/exec/ldst.c
@@ -15,17 +15,19 @@ make_EHelper(ld) {
 make_EHelper(lb){
   rtl_lm(&s0, &id_src->addr, decinfo.width);
   rtl_sext(&s1, &s0, 1); 
+  print_asm_template2(lb);
   rtl_sr(id_dest->reg, &s1, 4);  
 
-  print_asm_template2(lb);
+
 }
 
 make_EHelper(lh){
   rtl_lm(&s0, &id_src->addr, decinfo.width);
   rtl_sext(&s1, &s0, 2); 
+  print_asm_template2(lh);
   rtl_sr(id_dest->reg, &s1, 4);  
 
-  print_asm_template2(lh);
+
 }
 
 make_EHelper(st) {
