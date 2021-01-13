1cecf8c34828e12d93a18e36242e1d5022c4bdd7
tracer-ics2019 <tracer@njuics.org>
2021-01-13 08:04:59 -0800

    >  run U201711615 apple Linux ubuntu 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  08:04:59 up  4:10,  1 user,  load average: 0.40, 0.24, 0.20 c20c5bd172894a784a23c73aeb7b973c538d9bdb

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,17 @@
 #include "common.h"
+#include "syscall.h"
+
+extern _Context* do_syscall(_Context*);
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+  	case _EVENT_YIELD:
+  		printf("Here comes the yield event\n");
+  		break;
+  	case _EVENT_SYSCALL:
+  		printf("Here comes the system call\n");
+  		do_syscall(c);
+  		break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
