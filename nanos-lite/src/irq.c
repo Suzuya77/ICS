48a9ca96b463d8829ab6ab8ead69979b1f9c31ad
tracer-ics2019 <tracer@njuics.org>
2021-01-13 09:15:41 -0800

    >  run U201711615 apple Linux ubuntu 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  09:15:41 up  5:21,  1 user,  load average: 0.34, 0.40, 0.32 e59f082ee57945765642bd6e8a2672d5c7a80d1f

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,6 +6,7 @@ extern _Context* do_syscall(_Context*);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
   	case _EVENT_YIELD:
+  		Log("Here comes the yield event");
   		printf("Here comes the yield event\n");
   		break;
   	case _EVENT_SYSCALL:
