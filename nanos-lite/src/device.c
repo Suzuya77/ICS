9657406ec2ff266a89c48fd8b915b4bf0a00cf44
tracer-ics2019 <tracer@njuics.org>
2021-01-14 12:54:22 -0800

    >  compile U201711615 apple Linux ubuntu 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:54:22 up 15:34,  1 user,  load average: 0.22, 0.21, 0.19 d7b13c233b3b7b7ab109ee4eaa7152aef08e99

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -1,8 +1,19 @@
 #include "common.h"
 #include <amdev.h>
+#include <am.h>
+#include <nemu.h>
+
+extern int screen_width();
+extern int screen_height();
+
+size_t __am_input_read(uintptr_t reg, void *buf, size_t size);
+size_t __am_timer_read(uintptr_t reg, void *buf, size_t size);
+
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  for (size_t i = 0; i < len; ++i)
+    _putc(((char *)buf)[i]);
+  return len;
 }
 
 #define NAME(key) \
@@ -14,20 +25,36 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+    int keycode = read_key();
+    if(keycode != _KEY_NONE){
+      if (keycode & 0x8000) {
+        keycode ^= 0x8000;
+        len = sprintf(buf, "kd %s\n", keyname[keycode]);
+      } else if(!((keycode & ~0x8000) == _KEY_NONE)){
+        len = sprintf(buf, "ku %s\n", keyname[keycode]);
+      }
+    } else{
+      len = sprintf(buf, "t %u\n", uptime());
+    }
+    return len;
 }
 
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+    len = sprintf(buf, dispinfo + offset);
+  return len;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  int x = (offset / 4) % screen_width();
+  int y = (offset / 4) % screen_height();
+  draw_rect((void *)buf, x, y, len / 4, 1);
+  return len;
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
+  draw_sync();
   return 0;
 }
 
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -282,7 +282,7 @@ int fs_close(int fd){
 }
 
 void init_fs() {
-  // TODO: initialize the size of /dev/fb
+
   int fb = fs_open("/dev/fb", 0, 0);
   file_table[fb].size = screen_width()*screen_height()*sizeof(uint32_t);
   fb = fs_open("/dev/fbsync", 0, 0);
