c205ccd28dfc2527f924d43d10b8a9065ce24e02
tracer-ics2019 <tracer@njuics.org>
2021-01-14 17:16:22 -0800

    >  run U201711615 apple Linux ubuntu 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:16:22 up 2 min,  1 user,  load average: 0.49, 0.49, 0.22 ce069fb36fa71efbbdd1de6787db2144fe92c4d

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -1,205 +1,122 @@
-// #include "klib.h"
-
-// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
-// //use standard string.h for reference
-// size_t strlen(const char *s) {
-// 	register const char *temp = s;
-
-// 	while(*temp++);
-
-// 	return --temp - s;
-// }
-
-// char *strcpy(char* dst,const char* src) {
-// 	register char *temp = dst;
-
-// 	while(*temp++ = *src++);
-
-// 	return dst;
-// }
-
-// char* strncpy(char* dst, const char* src, size_t n) {
-// 	register char *temp = dst;
-
-// 	if (n > 0)
-// 	{
-// 		// n -= 1;
-// 		while((*temp++ = *src++) && (--n > 0));
-
-// 		if ((*--src == '\0') && (--n > 0))
-// 		{
-// 			while(n > 0){
-// 				*temp++ = '\0'; 
-// 				n--;
-// 			}
-// 		}
-// 	}
-// 	return dst;
-// }
-
-// char* strcat(char* dst, const char* src) {
-// 	register char *temp = dst;
-
-// 	while(*temp++ != '\0');
-
-// 	temp--;
-
-// 	while(*temp++ = *src++);
-
-// 	return dst;
-// }
-
-// int strcmp(const char* s1, const char* s2) {
-// 	while(*s1 == *s2){
-// 		if (*s1 == '\0')
-// 		{
-// 			return 0;
-// 		}
-// 		s1++;
-// 		s2++;
-// 	}
-
-// 	return (*(unsigned char*)s1 < *(unsigned char*)s2)?-1:1;
-// }
-
-// int strncmp(const char* s1, const char* s2, size_t n) {
-//   	while((*s1 == *s2) && n > 0){
-// 		if (*s1 == '\0')
-// 		{
-// 			return 0;
-// 		}
-// 		s1++;
-// 		s2++;
-// 		n--;
-// 	}
-
-// 	return (*(unsigned char*)s1 < *(unsigned char*)s2)?-1:1;
-// }
-
-// void* memset(void* v,int c,size_t n) {
-// 	register char *temp = v;
-
-// 	while(n > 0){
-// 		*temp = c;
-// 		temp++;
-// 		n--;
-// 	}
-
-// 	return v;
-// }
-
-// void* memcpy(void* out, const void* in, size_t n) {
-// 	register char *temp1 = out;
-// 	register const char *temp2 = in;
-
-// 	while(n > 0){
-// 		*temp1 = *temp2;
-// 		temp1++;
-// 		temp2++;
-// 		n--;
-// 	}
-
-// 	return out;
-// }
-
-// int memcmp(const void* s1, const void* s2, size_t n){
-// 	register const unsigned char *temp1 = s1, *temp2 = s2;
-
-// 	while(n > 0){
-// 		if (*temp1 == *temp2)
-// 		{
-// 			temp1++;
-// 			temp2++;
-// 			continue;
-// 		}else{
-// 			return *--temp1 - *--temp2;
-// 		}
-// 	}
-// 	return 0;
-// }
-
-// #endif
 #include "klib.h"
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+//use standard string.h for reference
 size_t strlen(const char *s) {
-	size_t len = 0;
-	while(s[len]!='\0'){
-		len++;
-	}
-  return len;
+	register const char *temp = s;
+
+	while(*temp++);
+
+	return --temp - s;
 }
 
 char *strcpy(char* dst,const char* src) {
-  size_t i;
-  for(i=0; src[i]!='\0'; i++){
-    dst[i] = src[i];
-	}
-  dst[i]='\0';
-  return dst;
+	register char *temp = dst;
+
+	while(*temp++ = *src++);
+
+	return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-	dst[n] = '\0';
-  size_t i;
-  for(i=0; src[i]!='\0' && i<n; i++){
-    dst[i] = src[i];
-	}
-  dst[++i]='\0';
-  return dst;
-}
+	register char *temp = dst;
 
-char* strcat(char* dst, const char* src) {
-  char *tmp = dst;
-  while(*dst){
-		dst++;
-	}
-  while((*dst++ = *src++) != '\0');
-  return tmp;
-}
+	if (n > 0)
+	{
+		// n -= 1;
+		while((*temp++ = *src++) && (--n > 0));
 
-int strcmp(const char* s1, const char* s2) {
-	while(*s1 && *s2 && *s1 == *s2){
-		s1++;
-		s2++;
-	}
-	return *s1 - *s2;
-}
-
-int strncmp(const char* s1, const char* s2, size_t n) {
-	while(--n && *s1 && *s2 && *s1 == *s2){
-		s1++;
-		s2++;
-	}
-	return *s1 - *s2;
-}
-
-void* memset(void* v,int c,size_t n) {
-	void *dst = v;
-	while(n--){
-		*(char *)v++ = (char)c;
+		if ((*--src == '\0') && (--n > 0))
+		{
+			while(n > 0){
+				*temp++ = '\0'; 
+				n--;
+			}
+		}
 	}
 	return dst;
 }
 
-void* memcpy(void* out, const void* in, size_t n) {
-  assert(out - in >= n);
-  size_t i;
-  for (i = 0; i < n; ++i){
-    ((uint8_t *)out)[i] = ((uint8_t *)in)[i];
-  }
-  return out;
+char* strcat(char* dst, const char* src) {
+	register char *temp = dst;
+
+	while(*temp++ != '\0');
+
+	temp--;
+
+	while(*temp++ = *src++);
+
+	return dst;
 }
 
-int memcmp(const void* s1, const void* s2, size_t n){
-	if(!n) return 0;
-	while(--n && *(char *)s1 == *(char *)s2){
+int strcmp(const char* s1, const char* s2) {
+	while(*s1 == *s2){
+		if (*s1 == '\0')
+		{
+			return 0;
+		}
 		s1++;
 		s2++;
 	}
-	return *(char *)s1 - *(char *)s2;
+
+	return (*(unsigned char*)s1 < *(unsigned char*)s2)?-1:1;
+}
+
+int strncmp(const char* s1, const char* s2, size_t n) {
+  	while((*s1 == *s2) && n > 0){
+		if (*s1 == '\0')
+		{
+			return 0;
+		}
+		s1++;
+		s2++;
+		n--;
+	}
+
+	return (*(unsigned char*)s1 < *(unsigned char*)s2)?-1:1;
+}
+
+void* memset(void* v,int c,size_t n) {
+	register char *temp = v;
+
+	while(n > 0){
+		*temp = c;
+		temp++;
+		n--;
+	}
+
+	return v;
+}
+
+void* memcpy(void* out, const void* in, size_t n) {
+	register char *temp1 = out;
+	register const char *temp2 = in;
+
+	while(n > 0){
+		*temp1 = *temp2;
+		temp1++;
+		temp2++;
+		n--;
+	}
+
+	return out;
+}
+
+int memcmp(const void* s1, const void* s2, size_t n){
+	register const unsigned char *temp1 = s1, *temp2 = s2;
+
+	while(n > 0){
+		if (*temp1 == *temp2)
+		{
+			temp1++;
+			temp2++;
+			continue;
+		}else{
+			return *--temp1 - *--temp2;
+		}
+	}
+	return 0;
 }
 
 #endif
