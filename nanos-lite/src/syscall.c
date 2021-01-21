5dd384f45c3f1148efd7336c7a4db209bd54dce8
tracer-ics2019 <tracer@njuics.org>
2021-01-21 06:43:35 -0800

    >  run U201711615 apple Linux ubuntu 5.4.0-62-generic #70~18.04.1-Ubuntu SMP Tue Jan 12 17:18:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  06:43:35 up  1:49,  1 user,  load average: 0.48, 0.43, 0.34 333c03578c41879516c9db7842d99c6167a4e20

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,13 +7,6 @@
 extern void naive_uload(PCB *pcb, const char *filename);
 static int programBrk;
 
-static inline int sys_open(const char*path, int flags, int mode);
-static inline int sys_close(int fd);
-static inline int sys_read(int fd, void*buf, size_t len);
-static inline int sys_write(int fd, const void*buf, size_t len);
-static inline int sys_lseek(int fd, size_t offset, int whence);
-static inline int sys_brk(int addr);
-
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -30,22 +23,23 @@ _Context* do_syscall(_Context *c) {
   		c->GPRx = 0;
   		break;
   	case SYS_write:
-  		c->GPRx = sys_write(a[1], (void *) (a[2]), a[3]);
+  		c->GPRx = fs_write(a[1], (void *) (a[2]), a[3]);
   		break;
     case SYS_read:
-      c->GPRx = sys_read(a[1], (void*)(a[2]), a[3]);
+      c->GPRx = fs_read(a[1], (void*)(a[2]), a[3]);
       break;
     case SYS_lseek:
-      c->GPRx = sys_lseek(a[1], a[2], a[3]);
+      c->GPRx = fs_lseek(a[1], a[2], a[3]);
       break;
     case SYS_open:
-      c->GPRx = sys_open((const char *)a[1], a[2], a[3]);
+      c->GPRx = fs_open((const char *)a[1], a[2], a[3]);
       break;
     case SYS_close:
-      c->GPRx = sys_close(a[1]);
+      c->GPRx = fs_close(a[1]);
       break;
     case SYS_brk:
-      c->GPRx = sys_brk(a[1]);
+      programBrk = a[1];
+      c->GPRx = 0;
       break;
     case SYS_execve:
       naive_uload(NULL, (const char*)a[1]);
@@ -55,29 +49,4 @@ _Context* do_syscall(_Context *c) {
   }
 
   return NULL;
-}
-
-static inline int sys_open(const char*path, int flags, int mode){
-    return fs_open(path, flags, mode);
-}
-
-static inline int sys_close(int fd){
-    return fs_close(fd);
-}
-
-static inline int sys_read(int fd, void*buf, size_t len){
-    return fs_read(fd, buf, len);
-}
-
-static inline int sys_write(int fd, const void*buf, size_t len){
-    return fs_write(fd, buf, len);
-}
-
-static inline int sys_lseek(int fd, size_t offset, int whence){
-    return fs_lseek(fd, offset, whence);
-}
-
-static inline int sys_brk(int addr){
-    programBrk = addr;
-    return 0;
 }
