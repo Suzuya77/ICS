55146645e296e53467ebb31219c2df5b05fd483f
tracer-ics2019 <tracer@njuics.org>
2021-01-21 07:43:46 -0800

    >  compile U201711615 apple Linux ubuntu 5.4.0-62-generic #70~18.04.1-Ubuntu SMP Tue Jan 12 17:18:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  07:43:46 up  2:50,  1 user,  load average: 0.64, 0.36, 0.30 55a3139682a730a4d857eb126a00f0b172cf88e0

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -2,19 +2,22 @@
 
 extern void raise_intr(uint32_t, vaddr_t);
 int32_t readcsr(int i){
+	uint32_t result;
 	switch(i){
 	  case 0x105:
-      return decinfo.isa.stvec;
+	  	result = decinfo.isa.stvec;
 	  case 0x142:
-      return decinfo.isa.scause;
+      	result =  decinfo.isa.scause;
 	  case 0x100:
-      return decinfo.isa.sstatus;
+      	result =  decinfo.isa.sstatus;
 	  case 0x141:
-      return decinfo.isa.sepc;
+      	result =  decinfo.isa.sepc;
 	  default:
-	  printf("Error Code: 0x%x\n", i);
-      assert(0);
+	  	printf("Error Code: 0x%x\n", i);
+      	assert(0);
 	}
+
+	return result;
 }
 
 void writecsr(int i, int32_t val){
@@ -43,29 +46,29 @@ make_EHelper(system){
 
 	switch(decinfo.isa.instr.funct3){
 		case 0b0:
-	    if((decinfo.isa.instr.val & ~(0x7f))==0){
-	        raise_intr(reg_l(17), decinfo.seq_pc - 4);
-	    }
-	    else if(decinfo.isa.instr.val == 0x10200073){
-	        decinfo.is_jmp = 1;
-	    }
-	    else{
-	        assert(0);
-	    }
-	    break;
+		    if((decinfo.isa.instr.val & ~(0x7f))==0){
+		        raise_intr(reg_l(17), decinfo.seq_pc - 4);
+		    }
+		    else if(decinfo.isa.instr.val == 0x10200073){
+		        decinfo.is_jmp = 1;
+		    }
+		    else{
+		        assert(0);
+		    }
+		    break;
 
 		case 0b001:
-	    s0 = readcsr(decinfo.isa.instr.csr);
-	    writecsr(decinfo.isa.instr.csr, id_src->val);
-	    rtl_sr(id_dest->reg, &s0, 4);
-	    break;
+		    s0 = readcsr(decinfo.isa.instr.csr);
+		    writecsr(decinfo.isa.instr.csr, id_src->val);
+		    rtl_sr(id_dest->reg, &s0, 4);
+		    break;
 	    
 		case 0b010:
-	    s0 = readcsr(decinfo.isa.instr.csr);
-	    writecsr(decinfo.isa.instr.csr, s0 | id_src->val);
-	    rtl_sr(id_dest->reg, &s0, 4);
-	    break;
+		    s0 = readcsr(decinfo.isa.instr.csr);
+		    writecsr(decinfo.isa.instr.csr, s0 | id_src->val);
+		    rtl_sr(id_dest->reg, &s0, 4);
+	    	break;
 		default:
-	    assert(0);
+	    	assert(0);
 	}
 }
